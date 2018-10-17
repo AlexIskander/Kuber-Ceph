d8edf6f389f5b579d414c7537cac82dd36283dc2
Alexsandr <1111-1111@ukr.net>
2018-10-17 16:32:55 +0300

    added Instalation kuburnetes to node and added list file repository kubernetes

---
+++ ansible/files/install_ceph.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~
+
+mkdir my-cluster
+cd  my-cluster
+ceph-deploy new node1 node2 node3
+echo public network = 192.168.77.0/24 >> ceph.conf
+ceph-deploy install node1 node2 node3
+ceph-deploy mon create-initial
+ceph-deploy admin node1 node2 node3
+
+ceph-deploy osd create  node1:/dev/sdc node2:/dev/sdc node3:/dev/sdc
